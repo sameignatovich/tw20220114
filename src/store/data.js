35114d85777a6070b3c328b3b4114932ef59b115
Nikita Ignatovich <nikita@ignatovich.me>
2022-01-17 12:53:37 +0300

    Split data to separated file

---
+++ src/store/data.js
@@ -0,0 +1,34 @@
+/* eslint "import/prefer-default-export": "off" */
+
+export const itemsData = [
+  {
+    date: '2022.01.14',
+    name: 'Наполнитель',
+    count: 243,
+    distance: 16,
+  },
+  {
+    date: '2022.01.15',
+    name: 'Когтеточки',
+    count: 4,
+    distance: 1023,
+  },
+  {
+    date: '2022.01.13',
+    name: 'Корм',
+    count: 14,
+    distance: 242,
+  },
+  {
+    date: '2022.01.06',
+    name: 'Лотки',
+    count: 56,
+    distance: 102,
+  },
+  {
+    date: '2022.01.28',
+    name: 'Игрушки',
+    count: 89,
+    distance: 341,
+  },
+];
--- src/store/index.js
+++ src/store/index.js
@@ -1,41 +1,11 @@
 import { createStore } from 'vuex';
 import filterItems from '@/misc/filter';
 import orderItems from '@/misc/order';
+import { itemsData } from '@/store/data';
 
 export default createStore({
   state: {
-    items: [
-      {
-        date: '2022.01.14',
-        name: 'Наполнитель',
-        count: 243,
-        distance: 16,
-      },
-      {
-        date: '2022.01.15',
-        name: 'Когтеточки',
-        count: 4,
-        distance: 1023,
-      },
-      {
-        date: '2022.01.13',
-        name: 'Корм',
-        count: 14,
-        distance: 242,
-      },
-      {
-        date: '2022.01.06',
-        name: 'Лотки',
-        count: 56,
-        distance: 102,
-      },
-      {
-        date: '2022.01.28',
-        name: 'Игрушки',
-        count: 89,
-        distance: 341,
-      },
-    ],
+    items: itemsData,
   },
   mutations: {
   },
